f140cb6e2f62c1b72f29662cf808f937aa7fca41
Dart <henryplaysingress@gmail.com>
2025-04-16 00:47:13 -0500

    Initial commit

---
+++ src/main/java/net/dart/graceofgod/GraceOfGodClient.java
@@ -0,0 +1,10 @@
+package net.dart.graceofgod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GraceOfGodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
